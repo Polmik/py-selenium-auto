77f6808ad0a91b3f26099b4e98f5cbeee1e66fb4
Egor R <mrpolmik@hotmail.com>
2023-11-08 15:04:15 +0300

    Version 0.3.4

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with _open("requirements.txt") as requirements_file:
     requirements = requirements_file.readlines()
 
 setuptools.setup(name="py-selenium-auto",
-                 version="0.4.0",
+                 version="0.3.4",
                  description="Selenium for Python",
                  long_description=description,
                  long_description_content_type="text/markdown",
