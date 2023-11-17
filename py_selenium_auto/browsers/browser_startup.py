d072de16fd53ce770dcb2fea74ded9dacb02b696
Egor R <mrpolmik@hotmail.com>
2023-11-17 10:06:33 +0300

    Updated work_dir

--- py_selenium_auto/browsers/browser_startup.py
+++ py_selenium_auto/browsers/browser_startup.py
@@ -1,14 +1,8 @@
-from pathlib import Path
 from typing import Callable, Optional
 
 from dependency_injector.providers import Factory, Self, Singleton
 from py_selenium_auto_core.applications.startup import ServiceProvider, Startup
 from py_selenium_auto_core.localization.localization_manager import LocalizationManager
-from py_selenium_auto_core.logging.logger import Logger
-from py_selenium_auto_core.utilities.environment_configuration import (
-    EnvironmentConfiguration,
-)
-from py_selenium_auto_core.utilities.file_reader import FileReader
 from py_selenium_auto_core.utilities.json_settings_file import JsonSettingsFile
 from py_selenium_auto_core.utilities.root_path_helper import RootPathHelper
 from py_selenium_auto_core.waitings.conditional_wait import ConditionalWait
@@ -29,7 +23,7 @@ class BrowserServiceProvider(ServiceProvider):
         LocalizationManager,
         ServiceProvider.logger_configuration,
         ServiceProvider.logger,
-        RootPathHelper.current_root_path(str(Path(__file__))),
+        RootPathHelper.current_root_path(__file__),
     )
 
     browser_profile: Singleton[BrowserProfile] = Singleton(
@@ -56,7 +50,10 @@ class BrowserStartup(Startup):
         service_provider: BrowserServiceProvider = None,
     ) -> BrowserServiceProvider:
         ServiceProvider.override(BrowserServiceProvider)
-        settings = settings or cls.get_settings()
+        settings = settings or cls.get_settings(
+            RootPathHelper.calling_root_path(),
+            RootPathHelper.current_root_path(__file__),
+        )
         service_provider: BrowserServiceProvider = super().configure_services(
             application_provider=application_provider,
             settings=settings,
@@ -66,18 +63,3 @@ class BrowserStartup(Startup):
 
         ServiceProvider.reset_override()
         return service_provider
-
-    @classmethod
-    def get_settings(cls) -> JsonSettingsFile:
-        profile_name = EnvironmentConfiguration.get_variable('profile')
-        settings_profile = 'settings.json' if not profile_name else f'settings.{profile_name}.json'
-        Logger.debug(f'Get settings from: {settings_profile}')
-        if FileReader.is_resource_file_exist(settings_profile, root_path=RootPathHelper.calling_root_path()):
-            return JsonSettingsFile(
-                setting_name=settings_profile,
-                root_path=RootPathHelper.calling_root_path(),
-            )
-        return JsonSettingsFile(
-            setting_name=settings_profile,
-            root_path=RootPathHelper.current_root_path(str(Path(__file__))),
-        )
