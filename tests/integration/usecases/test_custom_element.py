0feeb8ad7152da2b6dedb5950e86896ebad54c4e
Egor R <mrpolmik@hotmail.com>
2023-11-20 11:12:52 +0300

    Added new tests

---
+++ tests/integration/usecases/test_custom_element.py
@@ -0,0 +1,63 @@
+from py_selenium_auto_core.elements.constants.element_state import ElementState
+from py_selenium_auto_core.locator.locator import Locator
+
+from py_selenium_auto.browsers.browser_services import BrowserServices
+from py_selenium_auto.elements.text_box import TextBox
+from tests.integration.forms_test_app.the_internet.forms.authentication_form import AuthenticationForm
+from tests.integration.test_ui import TestUI
+
+
+class CustomTextBox(TextBox):
+    def __init__(self, locator: Locator, name: str, element_state: ElementState):
+        super().__init__(locator, name, ElementState.ExistsInAnyState)
+
+    @property
+    def text(self) -> str:
+        return self.value
+
+
+class TestCustomElement(TestUI):
+    form = AuthenticationForm()
+
+    def setup_method(self):
+        self.form.open()
+
+    def test_create_custom_text_box(self):
+        username_txb = self.form.user_name_text_box
+        username_custom_txb = CustomTextBox(username_txb.locator, username_txb.name)
+
+        username_txb.type('wrong')
+        username_custom_txb.type('right')
+
+        assert username_txb.value == username_custom_txb.text
+
+    def test_create_custom_text_box_via_element_factory(self):
+        username_txb = self.form.user_name_text_box
+        username_custom_txb = BrowserServices.Instance.service_provider.element_factory().get_custom_element(
+            CustomTextBox,
+            username_txb.locator,
+            username_txb.name,
+            ElementState.ExistsInAnyState,
+        )
+
+        username_txb.type('wrong')
+        username_custom_txb.type('right')
+
+        assert username_txb.value == username_custom_txb.text
+
+    def test_find_custom_text_box_via_element_factory(self):
+        username_txb = self.form.user_name_text_box
+        username_custom_txbs = BrowserServices.Instance.service_provider.element_factory().find_elements(
+            CustomTextBox,
+            username_txb.locator,
+            username_txb.name,
+            state=ElementState.ExistsInAnyState,
+        )
+
+        assert len(username_custom_txbs) == 1
+        username_custom_txb = username_custom_txbs[0]
+
+        username_txb.type('wrong')
+        username_custom_txb.type('right')
+
+        assert username_txb.value == username_custom_txb.text
---
+++ tests/integration/usecases/test_element_exists_but_not_displayed.py
@@ -0,0 +1,48 @@
+import pytest
+from py_selenium_auto_core.elements.constants.element_state import ElementState
+from py_selenium_auto_core.locator.locator import Locator
+from selenium.common import TimeoutException, NoSuchElementException
+from selenium.webdriver.common.by import By
+
+from py_selenium_auto.browsers.browser_services import BrowserServices
+from tests.integration.forms_test_app.the_internet.forms.hovers_form import HoversForm, HoverExample
+from tests.integration.test_ui import TestUI
+
+
+class TestElementExistsButNotDisplayed(TestUI):
+    form = HoversForm()
+    fake_element = Locator(By.XPATH, '//fake')
+    timeout = 1
+
+    def setup_method(self):
+        self.form.open()
+
+    def test_wait_for_element_which_exists_but_not_displayed(self):
+        button = self.form.get_hidden_element(HoverExample.First, ElementState.ExistsInAnyState)
+        assert BrowserServices.Instance.conditional_wait.wait_for_condition(
+            lambda: button.state.is_exist() and not button.state.is_displayed(),
+        )
+
+    def test_throw_web_driver_timeout_exception_when_element_not_in_desired_state(self):
+        try:
+            self.form.get_hidden_element(HoverExample.First, ElementState.Displayed).get_element(self.timeout)
+        except TimeoutException:
+            return
+        else:
+            pytest.fail('Expected TimeoutException')
+
+    @pytest.mark.parametrize(
+        argnames='state',
+        argvalues=[
+            pytest.param(ElementState.Displayed),
+            pytest.param(ElementState.ExistsInAnyState),
+        ],
+    )
+    def test_throw_no_such_element_exception_when_element_not_found_but_expected_to_be_displayed(self, state):
+        element_factory = BrowserServices.Instance.service_provider.element_factory()
+        try:
+            element_factory.get_button(self.fake_element, 'Fake', state).get_element(self.timeout)
+        except NoSuchElementException:
+            return
+        else:
+            pytest.fail(f'Expected NoSuchElementException for {state}')
